a35fff305a6f160f72d0bd0b0a7731d0a8ff422c
iren_airs <iren_airs@mail.ru>
2021-12-11 14:32:48 +0300

    first

---
+++ people.php
@@ -0,0 +1,26 @@
+<?php
+/***
+ * Тестовый класс для работы с базой людей
+ * Автор Ирина
+ * 09.12.2012
+ */
+namespace 
+class People
+{
+    private $id;
+    private $name;
+    private $surname;
+    private $birthday;
+    private $sex;
+    private $city_birth;
+
+
+    public function __construct($idOrPiole)
+    {
+
+    }
+
+    private
+
+
+}
